95d847dd17cfe7ffa47a43f4710418ea31d8dad4
Simon Waibl <elseym@me.com>
2013-02-08 10:29:57 +0100

    more git aliases

--- aliases-git.sh
+++ aliases-git.sh
@@ -6,12 +6,23 @@ alias ga='git add'
 alias gaa='ga .'
 
 alias gP='git push'
-alias gp='git pull --rebase'
-alias gpm='git pull'
 alias gpP='gp && gP'
 
 alias gc='git checkout'
 alias gr='git reset HEAD'
+
+alias gst='git stash'
+alias gsp='gst pop'
+
+function gp {
+	gs |grep "clean$" >/dev/null 2>&1
+	[ $? -eq 0 ] && git pull || git pull --rebase
+}
+
+function gaacm {
+	gaa
+	git commit -m "$*"
+}
 
 function gcm {
 	git commit -m "$*"
