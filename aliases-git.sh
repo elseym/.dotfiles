93428c8a717b614ed6e53cbeadc5ab2587f9b501
Simon Waibl <elseym@me.com>
2013-02-11 23:43:43 +0100

    fixed push and pull aliases

--- aliases-git.sh
+++ aliases-git.sh
@@ -5,19 +5,17 @@ alias gl='git lg'
 alias ga='git add'
 alias gaa='ga .'
 
+alias gp='git pull'
+alias gpr='git pull --rebase'
 alias gP='git push'
 alias gpP='gp && gP'
+alias gprP='gpr && gP'
 
 alias gc='git checkout'
 alias gr='git reset HEAD'
 
 alias gst='git stash'
 alias gsp='gst pop'
-
-function gp {
-	gs |grep "clean$" >/dev/null 2>&1
-	[ $? -eq 0 ] && git pull || git pull --rebase
-}
 
 function gaacm {
 	gaa
