590e49f52ed0436d2e4bf513221c06adc09108fc
Simon Waibl <elseym@me.com>
2013-05-02 08:54:45 +0200

    switch php via envvar

--- vars.sh
+++ vars.sh
@@ -1,5 +1,7 @@
-export TERM='xterm'
-export PATH="/usr/local/bin:/usr/local/sbin:`echo $PATH | sed 's/\/usr\/local\/bin\:*//g'`:/usr/local/share/npm/bin"
+export PHPVER='php54'
+
+export TERM='xterm-256color'
+export PATH="$(brew --prefix $PHPVER)/bin:/usr/local/bin:/usr/local/sbin:`echo $PATH | sed 's/\/usr\/local\/bin\:*//g'`:/usr/local/share/npm/bin"
 export NODE_PATH="/usr/local/lib/node_modules:/usr/local/lib/node_modules/npm"
 
 export GIT_EDITOR=nano
@@ -11,5 +13,4 @@ export HISTCONTROL=ignoredups
 export LESS="-erX"
 
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
-[ -n "$TMUX" ] && export TERM='screen-256color'
-
+[ -n "$TMUX" ] && export TERM='xterm-256color'
