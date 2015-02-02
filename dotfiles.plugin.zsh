ed8b0987fa6854c0a466aa5823b4345f2ece8228
elseym <elseym@me.com>
2015-02-02 10:59:49 +0100

    better setup

--- dotfiles.plugin.zsh
+++ dotfiles.plugin.zsh
@@ -1,12 +1,12 @@
-BASEDIR=${0%/*.*}
+#!/bin/zsh
+
+BASEDIR=${0%/*}
 
 if [ ! -L ${HOME}/.zshrc ]; then
   [ -f ${HOME}/.zshrc ] && mv ${HOME}/.zshrc ${HOME}/.zshrc.original
   ln -sf ${BASEDIR}/.zshrc ${HOME}
 fi
 
-# set bundles
-export ELSEYM_ANTIGEN_BUNDLES=$(cat ${BASEDIR}/bundles.antigen)
+antigen bundles <<<$(cat ${BASEDIR}/bundles.antigen)
 
-# set theme
-export ELSEYM_DOTFILES_THEME=$(cat ${BASEDIR}/theme.antigen)
+antigen theme "$(cat ${BASEDIR}/theme.antigen)"
