614bc6f6496de59751e201df9b1dbc1d2db20b3b
elseym <elseym@me.com>
2015-03-05 09:06:43 +0100

    add documentation

--- functions.zsh
+++ functions.zsh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+##
+# elunalias - semantic unalias
+# deletes all alias-definitions containing verb and starting with prefix.
+# prefix defaults to the first letter of verb.
+#  usage: elunalias verb [prefix]
+#
 function elunalias {
   [ "${1}" -a "${2=${1:0:1}}" ] && \
     alias |grep -E "^'?${2}.*${1}" |cut -d= -f1 |\
